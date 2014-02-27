417d2fe350ff1287b62619dc4253df2e48dc0ea7
小傅Fox <xfox@cotr.me>
2014-02-27 19:40:13 +0800

    2014年2月27日[1] 1.修正了之前版本的所有BUGs 2.增加了2个接口对应的函数
    //TODO:增加提交图片的函数

--- api.php
+++ api.php
@@ -4,6 +4,8 @@
  *
  * @author 小傅Fox[<xfox@cotr.me>]
  */
+include_once './curl.php';
+define( 'DEBUG' , true );
 class WeiboEx extends Exception
 {
 
@@ -22,6 +24,20 @@ class WeiboPHP
      * @var string
      */
     private $host = 'https://api.weibo.com/2/';
+    /**
+     * 构造函数
+     *
+     * @param string $username
+     *            用户名
+     * @param string $password
+     *            密码
+     * @param int $app_key
+     *            App Key
+     * @param string $r_uri
+     *            redirect_uri
+     *            
+     * @throws WeiboEx
+     */
     public function __construct( $username , $password , $app_key , $r_uri )
     {
         $post_arr = array( 
@@ -39,10 +55,10 @@ class WeiboPHP
                 'passwd' => $password 
         );
         $header = array( 
-                'Referer: https://api.weibo.com/oauth2/authorize?client_id=' . $app_key . '&redirect_uri=' . $r_uri 
+                'Referer' => 'https://api.weibo.com/oauth2/authorize?client_id=' . $app_key . '&redirect_uri=' . $r_uri 
         );
         $r = WeiboPHP_cURL_Post( 'https://api.weibo.com/oauth2/authorize' , $post_arr , $header , 2 );
-        if( strpos( $r , 'HTTP/1.1 200 OK' ) ){
+        if( ! strpos( $r , 'HTTP/1.1 200 OK' ) ){
             $r_uri = str_replace( '/' , '\/' , $r_uri );
             preg_match( '/Location: ' . $r_uri . '#access_token=.*&remind_in=/' , $r , $match );
             $r_uri = str_replace( '\/' , '/' , $r_uri );
@@ -53,21 +69,89 @@ class WeiboPHP
             throw new WeiboEx( 'Authorize error.' . $r );
         }
     }
-    private function HTTPPost( $url , $post , $header )
+    private function HTTPPost( $url , $post , $header = NULL )
     {
         $url = $this -> host . $url;
         $post[ 'access_token' ] = $this -> token;
-        return json_decode( WeiboPHP_cURL_Post( $url , $post , $header , 0 ) , JSON_OBJECT_AS_ARRAY );
+        $r = json_decode( WeiboPHP_cURL_Post( $url , $post , $header , 0 ) , true );
+        if( json_last_error() > 0 ){
+            throw new Exception( 'Json Error' . json_last_error() );
+        }
+        if( isset( $r[ 'error_code' ] ) ){
+            throw new Exception( $r[ 'error' ] , $r[ 'error_code' ] );
+        }
+        return $r;
     }
-    private function HTTPGet( $url , $get , $header )
+    private function HTTPGet( $url , $get , $header = NULL )
     {
         $url = $this -> host . $url;
         $get[ 'access_token' ] = $this -> token;
-        return json_decode( WeiboPHP_cURL_Get( $url , $get , $header , 0 ) , JSON_OBJECT_AS_ARRAY );
+        $r = json_decode( WeiboPHP_cURL_Get( $url , $get , $header , 0 ) , true );
+        if( json_last_error() > 0 ){
+            throw new Exception( 'Json Error' . json_last_error() );
+        }
+        if( isset( $r[ 'error_code' ] ) ){
+            throw new Exception( $r[ 'error' ] , $r[ 'error_code' ] );
+        }
+        return $r;
     }
+    /**
+     * 获取Access Token
+     *
+     * @return string
+     */
     public function GetToken()
     {
         return $this -> token;
     }
-
+    /**
+     * 返回最新的200条公共微博，返回结果非完全实时
+     * Wiki:{@link http://open.weibo.com/wiki/2/statuses/public_timeline}
+     *
+     * @param int $count
+     *            单页返回的记录条数，最大不超过200，默认为20。
+     *            
+     * @return array
+     */
+    public function statuses__public_timeline( $count = 20 )
+    {
+        $get = array( 
+                'count' => $count 
+        );
+        return $this -> HTTPGet( 'statuses/public_timeline.json' , $get );
+    }
+    /**
+     * 获取当前登录用户及其所关注用户的最新微博
+     * Wiki:{@link http://open.weibo.com/wiki/2/statuses/friends_timeline}
+     *
+     * @param int $since_id
+     *            若指定此参数，则返回ID比since_id大的微博（即比since_id时间晚的微博），默认为0。
+     * @param int $max_id
+     *            若指定此参数，则返回ID小于或等于max_id的微博，默认为0。
+     * @param int $count
+     *            单页返回的记录条数，最大不超过100，默认为20。
+     * @param int $page
+     *            返回结果的页码，默认为1。
+     * @param int $base_app
+     *            是否只获取当前应用的数据。0为否（所有数据），1为是（仅当前应用），默认为0。
+     * @param int $feature
+     *            过滤类型ID，0：全部、1：原创、2：图片、3：视频、4：音乐，默认为0。
+     * @param int $trim_user
+     *            返回值中user字段开关，0：返回完整user字段、1：user字段仅返回user_id，默认为0。
+     *            
+     * @return array
+     */
+    public function statuses__friends_timeline( $since_id = 0 , $max_id = 0 , $count = 20 , $page = 1 , $base_app = 0 , $feature = 0 , $trim_user = 0 )
+    {
+        $get = array( 
+                'since_id' => $since_id , 
+                'max_id' => $max_id , 
+                'count' => $count , 
+                'page' => $page , 
+                'base_app' => $base_app , 
+                'feature' => $feature , 
+                'trim_user' => $trim_user 
+        );
+        return $this -> HTTPGet( 'statuses/friends_timeline.json' , $get );
+    }
 }
--- curl.php
+++ curl.php
@@ -4,31 +4,52 @@
  *
  * @author 小傅Fox[<xfox@cotr.me>]
  */
-function WeiboPHP_cURL_Post( $url , $post , $header , $header_type = '0' )
+function WeiboPHP_cURL_Post( $url , $post , $header = NULL , $header_type = '0' )
 {
     // init
     $ch = curl_init( $url );
     // set post string
+    $f_post = '';
     foreach( $post as $key => $value ){
         $f_post = $f_post . '&' . urlencode( $key ) . '=' . urlencode( $value );
     }
     $f_post = substr( $f_post , 1 );
-    // set header
-    $i = 0;
-    foreach( $header as $key => $value ){
-        $f_header[ $i ] = "{$key}: {$value}";
-        $i ++;
+    /* ===DEBUG=== */
+    if( DEBUG ){
+        echo $f_post;
     }
-    $f_header[ $i ] = 'Content-Type: application/x-www-form-urlencoded';
+    // set header
+    if( $header != NULL ){
+        $i = 0;
+        $f_header = array();
+        foreach( $header as $key => $value ){
+            $f_header[ $i ] = "{$key}: {$value}";
+            $i ++;
+        }
+    }
+    // $f_header[ $i ] = 'Content-Type: application/x-www-form-urlencoded';
+    /* ===DEBUG=== */
+    if( DEBUG ){
+        var_dump( $f_header );
+    }
     // setopt
     curl_setopt( $ch , CURLOPT_POST , 1 );
-    curl_setopt( $ch , CURLOPT_POSTFIELDS , $post );
+    curl_setopt( $ch , CURLOPT_POSTFIELDS , $f_post );
     curl_setopt( $ch , CURLOPT_SSL_VERIFYPEER , FALSE );
     curl_setopt( $ch , CURLOPT_SSL_VERIFYHOST , FALSE );
     curl_setopt( $ch , CURLOPT_RETURNTRANSFER , 1 );
-    curl_setopt( $ch , CURLOPT_HTTPHEADER , $header );
+    if( isset( $f_header ) ){
+        curl_setopt( $ch , CURLOPT_HTTPHEADER , $f_header );
+    }
     curl_setopt( $ch , CURLOPT_FOLLOWLOCATION , 0 );
     curl_setopt( $ch , CURLOPT_HEADER , $header_type );
+    /* ===DEBUG=== */
+    if( DEBUG ){
+        // curl_setopt($ch,CURLOPT_HTTPPROXYTUNNEL,true);
+        curl_setopt( $ch , CURLOPT_PROXY , '127.0.0.1:8888' );
+        // curl_setopt($ch, CURLOPT_PROXYPORT, '8080');
+    }
+    /* !!!DEBUG!!! */
     // exec
     $r = curl_exec( $ch );
     if( curl_errno( $ch ) != 0 ){
@@ -37,28 +58,40 @@ function WeiboPHP_cURL_Post( $url , $post , $header , $header_type = '0' )
     // return
     return $r;
 }
-function WeiboPHP_cURL_Get( $url , $get , $header , $header_type = '0' )
+function WeiboPHP_cURL_Get( $url , $get , $header = NULL , $header_type = '0' )
 {
-    // init
-    $ch = curl_init( $url );
     // set post string
+    $f_get = '';
     foreach( $get as $key => $value ){
         $f_get = $f_get . '&' . urlencode( $key ) . '=' . urlencode( $value );
     }
     $f_get = substr( $f_get , 1 );
     // set header
-    $i = 0;
-    foreach( $header as $key => $value ){
-        $f_header[ $i ] = "{$key}: {$value}";
-        $i ++;
+    if( $header != NULL ){
+        $i = 0;
+        foreach( $header as $key => $value ){
+            $f_header[ $i ] = "{$key}: {$value}";
+            $i ++;
+        }
     }
+    // init
+    $ch = curl_init( $url . '?' . $f_get );
     // setopt
     curl_setopt( $ch , CURLOPT_SSL_VERIFYPEER , FALSE );
     curl_setopt( $ch , CURLOPT_SSL_VERIFYHOST , FALSE );
     curl_setopt( $ch , CURLOPT_RETURNTRANSFER , 1 );
-    curl_setopt( $ch , CURLOPT_HTTPHEADER , $header );
+    if( isset( $f_header ) ){
+        curl_setopt( $ch , CURLOPT_HTTPHEADER , $f_header );
+    }
     curl_setopt( $ch , CURLOPT_FOLLOWLOCATION , 0 );
     curl_setopt( $ch , CURLOPT_HEADER , $header_type );
+    /* ===DEBUG=== */
+    if( DEBUG ){
+        // curl_setopt($ch,CURLOPT_HTTPPROXYTUNNEL,true);
+        curl_setopt( $ch , CURLOPT_PROXY , '127.0.0.1:8888' );
+        // curl_setopt($ch, CURLOPT_PROXYPORT, '8080');
+    }
+    /* !!!DEBUG!!! */
     // exec
     $r = curl_exec( $ch );
     if( curl_errno( $ch ) != 0 ){
