14133f7deb09c5751d5e48f0db633381f6ef8cb3
小傅Fox <xfox@cotr.me>
2014-06-22 16:47:48 +0800

    Allow single token to construct

--- api.php
+++ api.php
@@ -5,12 +5,9 @@
  * @author 小傅Fox[<xfox@cotr.me>]
  */
 include_once './curl.php';
-class WeiboEx extends Exception
-{
-
+class WeiboEx extends Exception{
 }
-class WeiboPHP
-{
+class WeiboPHP{
     /**
      * access token
      *
@@ -52,14 +49,15 @@ class WeiboPHP
      *
      * @ throws WeiboEx
      */
-    public function __construct()
-    {
+    public function __construct(){
         $args = func_get_args();
-        if( count( $args ) == 4 ){
-            $this -> __construct0( $args[ 0 ] , $args[ 1 ] , $args[ 2 ] , $args[ 3 ] );
-        } elseif( count( $args ) == 5 ){
-            $this -> __construct1( $args[ 0 ] , $args[ 1 ] , $args[ 2 ] , $args[ 3 ] , $args[ 4 ] );
-        } else{
+        if (count( $args ) == 4) {
+            $this->__construct0( $args [0], $args [1], $args [2], $args [3] );
+        } elseif (count( $args ) == 5) {
+            $this->__construct1( $args [0], $args [1], $args [2], $args [3], $args [4] );
+        } elseif (count( $args ) == 1) {
+            $this->__construct2( $args [0] );
+        } else {
             throw new Exception( 'Construct args error!' );
         }
     }
@@ -77,35 +75,34 @@ class WeiboPHP
      *            
      * @throws WeiboEx
      */
-    private function __construct0( $username , $password , $app_key , $r_uri )
-    {
-        $post_arr = array( 
-                'client_id' => $app_key , 
-                'redirect_uri' => $r_uri , 
-                'action' => 'submit' , 
-                'response_type' => 'token' , 
-                'isLoginSina' => '0' , 
-                'from' => '' , 
-                'regCallback' => '' , 
-                'state' => '' , 
-                'ticket' => '' , 
-                'withOfficalFlag' => '0' , 
-                'userId' => $username , 
-                'passwd' => $password , 
+    private function __construct0($username, $password, $app_key, $r_uri){
+        $post_arr = array (
+                'client_id' => $app_key,
+                'redirect_uri' => $r_uri,
+                'action' => 'submit',
+                'response_type' => 'token',
+                'isLoginSina' => '0',
+                'from' => '',
+                'regCallback' => '',
+                'state' => '',
+                'ticket' => '',
+                'withOfficalFlag' => '0',
+                'userId' => $username,
+                'passwd' => $password,
                 'scope' => 'all' 
         );
-        $header = array( 
+        $header = array (
                 'Referer' => 'https://api.weibo.com/oauth2/authorize?client_id=' . $app_key . '&redirect_uri=' . $r_uri . '&scope=all&response_type=token' 
         );
-        $r = WeiboPHP_cURL_Post( 'https://api.weibo.com/oauth2/authorize' , $post_arr , $header , 2 );
-        if( ! strpos( $r , 'HTTP/1.1 200 OK' ) ){
-            $r_uri = str_replace( '/' , '\/' , $r_uri );
-            preg_match( '/Location: ' . $r_uri . '#access_token=.*&remind_in=/' , $r , $match );
-            $r_uri = str_replace( '\/' , '/' , $r_uri );
-            $rt = str_replace( 'Location: ' . $r_uri . '#access_token=' , '' , $match[ 0 ] );
-            $rt = str_replace( '&remind_in=' , '' , $rt );
+        $r = WeiboPHP_cURL_Post( 'https://api.weibo.com/oauth2/authorize', $post_arr, $header, 2 );
+        if (! strpos( $r, 'HTTP/1.1 200 OK' )) {
+            $r_uri = str_replace( '/', '\/', $r_uri );
+            preg_match( '/Location: ' . $r_uri . '#access_token=.*&remind_in=/', $r, $match );
+            $r_uri = str_replace( '\/', '/', $r_uri );
+            $rt = str_replace( 'Location: ' . $r_uri . '#access_token=', '', $match [0] );
+            $rt = str_replace( '&remind_in=', '', $rt );
             $this -> token = $rt;
-        } else{
+        } else {
             throw new WeiboEx( 'Authorize error.' . $r );
         }
     }
@@ -125,61 +122,63 @@ class WeiboPHP
      *            
      * @throws WeiboEx
      */
-    private function __construct1( $username , $password , $app_key , $app_secret , $r_uri )
-    {
-        $post_arr = array( 
-                'client_id' => $app_key , 
-                'redirect_uri' => $r_uri , 
-                'action' => 'submit' , 
-                'response_type' => 'code' , 
-                'isLoginSina' => '0' , 
-                'from' => '' , 
-                'regCallback' => '' , 
-                'state' => '' , 
-                'ticket' => '' , 
-                'withOfficalFlag' => '0' , 
-                'userId' => $username , 
-                'passwd' => $password , 
+    private function __construct1($username, $password, $app_key, $app_secret, $r_uri){
+        $post_arr = array (
+                'client_id' => $app_key,
+                'redirect_uri' => $r_uri,
+                'action' => 'submit',
+                'response_type' => 'code',
+                'isLoginSina' => '0',
+                'from' => '',
+                'regCallback' => '',
+                'state' => '',
+                'ticket' => '',
+                'withOfficalFlag' => '0',
+                'userId' => $username,
+                'passwd' => $password,
                 'scope' => 'all' 
         );
-        $header = array( 
+        $header = array (
                 'Referer' => 'https://api.weibo.com/oauth2/authorize?client_id=' . $app_key . '&redirect_uri=' . $r_uri . '&scope=all&response_type=code' 
         );
-        $r = WeiboPHP_cURL_Post( 'https://api.weibo.com/oauth2/authorize' , $post_arr , $header , 2 );
-        if( ! strpos( $r , 'HTTP/1.1 200 OK' ) ){
+        $r = WeiboPHP_cURL_Post( 'https://api.weibo.com/oauth2/authorize', $post_arr, $header, 2 );
+        if (! strpos( $r, 'HTTP/1.1 200 OK' )) {
             $p_r_uri = parse_url( $r_uri );
-            if( ! isset( $p_r_uri[ 'query' ] ) ){
-                $r_uri = str_replace( '/' , '\/' , $r_uri );
-                preg_match( '/Location: ' . $r_uri . '\?code=[^\s]*/' , $r , $match );
-                $r_uri = str_replace( '\/' , '/' , $r_uri );
-                $rtc = str_replace( 'Location: ' . $r_uri . '?code=' , '' , $match[ 0 ] );
-                $post = array( 
-                        'client_id' => $app_key , 
-                        'client_secret' => $app_secret , 
-                        'grant_type' => 'authorization_code' , 
-                        'code' => $rtc , 
+            if (! isset( $p_r_uri ['query'] )) {
+                $r_uri = str_replace( '/', '\/', $r_uri );
+                preg_match( '/Location: ' . $r_uri . '\?code=[^\s]*/', $r, $match );
+                $r_uri = str_replace( '\/', '/', $r_uri );
+                $rtc = str_replace( 'Location: ' . $r_uri . '?code=', '', $match [0] );
+                $post = array (
+                        'client_id' => $app_key,
+                        'client_secret' => $app_secret,
+                        'grant_type' => 'authorization_code',
+                        'code' => $rtc,
                         'redirect_uri' => $r_uri 
                 );
-                $rt = json_decode( WeiboPHP_cURL_Post( 'https://api.weibo.com/oauth2/access_token' , $post ) , true );
-                $this -> token = $rt[ 'access_token' ];
-            } else{
-                $r_uri = str_replace( '/' , '\/' , $r_uri );
-                preg_match( '/Location: ' . $r_uri . '&code=[^\s]*/' , $r , $match );
-                $r_uri = str_replace( '\/' , '/' , $r_uri );
-                $rtc = str_replace( 'Location: ' . $r_uri . '&code=' , '' , $match[ 0 ] );
-                $post = array( 
-                        'client_id' => $app_key , 
-                        'client_secret' => $app_secret , 
-                        'grant_type' => 'authorization_code' , 
-                        'code' => $rtc , 
+                $rt = json_decode( WeiboPHP_cURL_Post( 'https://api.weibo.com/oauth2/access_token', $post ), true );
+                $this -> token = $rt ['access_token'];
+            } else {
+                $r_uri = str_replace( '/', '\/', $r_uri );
+                preg_match( '/Location: ' . $r_uri . '&code=[^\s]*/', $r, $match );
+                $r_uri = str_replace( '\/', '/', $r_uri );
+                $rtc = str_replace( 'Location: ' . $r_uri . '&code=', '', $match [0] );
+                $post = array (
+                        'client_id' => $app_key,
+                        'client_secret' => $app_secret,
+                        'grant_type' => 'authorization_code',
+                        'code' => $rtc,
                         'redirect_uri' => $r_uri 
                 );
-                $rt = json_decode( WeiboPHP_cURL_Post( 'https://api.weibo.com/oauth2/access_token' , $post ) , true );
-                $this -> token = $rt[ 'access_token' ];
+                $rt = json_decode( WeiboPHP_cURL_Post( 'https://api.weibo.com/oauth2/access_token', $post ), true );
+                $this -> token = $rt ['access_token'];
             }
-        } else{
+        } else {
             throw new WeiboEx( 'Authorize error.' . $r );
         }
+    }
+    private function __construct2($token){
+        $this -> token = $token;
     }
     /**
      * 以POST方法进行接口请求
@@ -194,16 +193,15 @@ class WeiboPHP
      * @throws Exception
      * @return string
      */
-    public function HTTPPost( $url , $post , $header = NULL )
-    {
+    public function HTTPPost($url, $post, $header = NULL){
         $url = $this -> host . $url;
-        $post[ 'access_token' ] = $this -> token;
-        $r = json_decode( WeiboPHP_cURL_Post( $url , $post , $header , 0 ) , true );
-        if( json_last_error() > 0 ){
+        $post ['access_token'] = $this -> token;
+        $r = json_decode( WeiboPHP_cURL_Post( $url, $post, $header, 0 ), true );
+        if (json_last_error() > 0) {
             throw new Exception( 'Json Error' . json_last_error() );
         }
-        if( isset( $r[ 'error_code' ] ) ){
-            throw new Exception( $r[ 'error' ] , $r[ 'error_code' ] );
+        if (isset( $r ['error_code'] )) {
+            throw new Exception( $r ['error'], $r ['error_code'] );
         }
         return $r;
     }
@@ -220,16 +218,15 @@ class WeiboPHP
      * @throws Exception
      * @return string
      */
-    public function HTTPGet( $url , $get , $header = NULL )
-    {
+    public function HTTPGet($url, $get, $header = NULL){
         $url = $this -> host . $url;
-        $get[ 'access_token' ] = $this -> token;
-        $r = json_decode( WeiboPHP_cURL_Get( $url , $get , $header , 0 ) , true );
-        if( json_last_error() > 0 ){
+        $get ['access_token'] = $this -> token;
+        $r = json_decode( WeiboPHP_cURL_Get( $url, $get, $header, 0 ), true );
+        if (json_last_error() > 0) {
             throw new Exception( 'Json Error' . json_last_error() );
         }
-        if( isset( $r[ 'error_code' ] ) ){
-            throw new Exception( $r[ 'error' ] , $r[ 'error_code' ] );
+        if (isset( $r ['error_code'] )) {
+            throw new Exception( $r ['error'], $r ['error_code'] );
         }
         return $r;
     }
@@ -246,16 +243,15 @@ class WeiboPHP
      * @throws Exception
      * @return string
      */
-    public function HTTPPostArr( $url , $post , $header = NULL )
-    {
+    public function HTTPPostArr($url, $post, $header = NULL){
         $url = $this -> host . $url;
-        $post[ 'access_token' ] = $this -> token;
-        $r = json_decode( WeiboPHP_cURL_PostArr( $url , $post , $header , 0 ) , true );
-        if( json_last_error() > 0 ){
+        $post ['access_token'] = $this -> token;
+        $r = json_decode( WeiboPHP_cURL_PostArr( $url, $post, $header, 0 ), true );
+        if (json_last_error() > 0) {
             throw new Exception( 'Json Error' . json_last_error() );
         }
-        if( isset( $r[ 'error_code' ] ) ){
-            throw new Exception( $r[ 'error' ] , $r[ 'error_code' ] );
+        if (isset( $r ['error_code'] )) {
+            throw new Exception( $r ['error'], $r ['error_code'] );
         }
         return $r;
     }
@@ -264,8 +260,7 @@ class WeiboPHP
      *
      * @return string
      */
-    public function GetToken()
-    {
+    public function GetToken(){
         return $this -> token;
     }
     /**
@@ -277,12 +272,11 @@ class WeiboPHP
      *            
      * @return array
      */
-    public function statuses__public_timeline( $count = 20 )
-    {
-        $get = array( 
+    public function statuses__public_timeline($count = 20){
+        $get = array (
                 'count' => $count 
         );
-        return $this -> HTTPGet( 'statuses/public_timeline.json' , $get );
+        return $this->HTTPGet( 'statuses/public_timeline.json', $get );
     }
     /**
      * 获取当前登录用户及其所关注用户的最新微博
@@ -305,18 +299,17 @@ class WeiboPHP
      *            
      * @return array
      */
-    public function statuses__friends_timeline( $since_id = 0 , $max_id = 0 , $count = 20 , $page = 1 , $base_app = 0 , $feature = 0 , $trim_user = 0 )
-    {
-        $get = array( 
-                'since_id' => $since_id , 
-                'max_id' => $max_id , 
-                'count' => $count , 
-                'page' => $page , 
-                'base_app' => $base_app , 
-                'feature' => $feature , 
+    public function statuses__friends_timeline($since_id = 0, $max_id = 0, $count = 20, $page = 1, $base_app = 0, $feature = 0, $trim_user = 0){
+        $get = array (
+                'since_id' => $since_id,
+                'max_id' => $max_id,
+                'count' => $count,
+                'page' => $page,
+                'base_app' => $base_app,
+                'feature' => $feature,
                 'trim_user' => $trim_user 
         );
-        return $this -> HTTPGet( 'statuses/friends_timeline.json' , $get );
+        return $this->HTTPGet( 'statuses/friends_timeline.json', $get );
     }
     /**
      * 获取当前登录用户及其所关注用户的最新微博
@@ -339,18 +332,17 @@ class WeiboPHP
      *            
      * @return array
      */
-    public function statuses__home_timeline( $since_id = 0 , $max_id = 0 , $count = 20 , $page = 1 , $base_app = 0 , $feature = 0 , $trim_user = 0 )
-    {
-        $get = array( 
-                'since_id' => $since_id , 
-                'max_id' => $max_id , 
-                'count' => $count , 
-                'page' => $page , 
-                'base_app' => $base_app , 
-                'feature' => $feature , 
+    public function statuses__home_timeline($since_id = 0, $max_id = 0, $count = 20, $page = 1, $base_app = 0, $feature = 0, $trim_user = 0){
+        $get = array (
+                'since_id' => $since_id,
+                'max_id' => $max_id,
+                'count' => $count,
+                'page' => $page,
+                'base_app' => $base_app,
+                'feature' => $feature,
                 'trim_user' => $trim_user 
         );
-        return $this -> HTTPGet( 'statuses/home_timeline.json' , $get );
+        return $this->HTTPGet( 'statuses/home_timeline.json', $get );
     }
     /**
      * 获取当前登录用户及其所关注用户的最新微博的ID
@@ -371,17 +363,16 @@ class WeiboPHP
      *            
      * @return array
      */
-    public function statuses__friends_timeline__ids( $since_id = 0 , $max_id = 0 , $count = 20 , $page = 1 , $base_app = 0 , $feature = 0 )
-    {
-        $get = array( 
-                'since_id' => $since_id , 
-                'max_id' => $max_id , 
-                'count' => $count , 
-                'page' => $page , 
-                'base_app' => $base_app , 
+    public function statuses__friends_timeline__ids($since_id = 0, $max_id = 0, $count = 20, $page = 1, $base_app = 0, $feature = 0){
+        $get = array (
+                'since_id' => $since_id,
+                'max_id' => $max_id,
+                'count' => $count,
+                'page' => $page,
+                'base_app' => $base_app,
                 'feature' => $feature 
         );
-        return $this -> HTTPGet( 'statuses/friends_timeline/ids' , $get );
+        return $this->HTTPGet( 'statuses/friends_timeline/ids', $get );
     }
     /**
      * 获取某个用户最新发表的微博列表
@@ -412,24 +403,23 @@ class WeiboPHP
      *            
      * @return array
      */
-    public function statuses__user_timeline( $uid = '' , $screen_name = '' , $since_id = 0 , $max_id = 0 , $count = 20 , $page = 1 , $base_app = 0 , $feature = 0 , $trim_user = 0 )
-    {
-        $get = array( 
+    public function statuses__user_timeline($uid = '', $screen_name = '', $since_id = 0, $max_id = 0, $count = 20, $page = 1, $base_app = 0, $feature = 0, $trim_user = 0){
+        $get = array (
                 // 'uid'=>$uid,
                 // 'screen_name'=>$screen_name,
-                'since_id' => $since_id , 
-                'max_id' => $max_id , 
-                'count' => $count , 
-                'page' => $page , 
-                'base_app' => $base_app , 
-                'feature' => $feature , 
+                'since_id' => $since_id,
+                'max_id' => $max_id,
+                'count' => $count,
+                'page' => $page,
+                'base_app' => $base_app,
+                'feature' => $feature,
                 'trim_user' => $trim_user 
         );
-        if( $uid == '' ){
-            $get[ 'screen_name' ] = $screen_name;
-        } elseif( $screen_name == '' ){
-            $get[ 'uid' ] = $uid;
+        if ($uid == '') {
+            $get ['screen_name'] = $screen_name;
+        } elseif ($screen_name == '') {
+            $get ['uid'] = $uid;
         }
-        return $this -> HTTPGet( 'statuses/user_timeline' , $get );
+        return $this->HTTPGet( 'statuses/user_timeline', $get );
     }
 }
--- curl.php
+++ curl.php
@@ -14,40 +14,39 @@
  * @throws Exception
  * @return mixed
  */
-function WeiboPHP_cURL_Post( $url , $post , $header = NULL , $header_type = '0' )
-{
+function WeiboPHP_cURL_Post($url, $post, $header = NULL, $header_type = '0'){
     // init
     $ch = curl_init( $url );
     // set post string
     $f_post = '';
-    foreach( $post as $key => $value ){
+    foreach ( $post as $key => $value ) {
         $f_post = $f_post . '&' . urlencode( $key ) . '=' . urlencode( $value );
     }
-    $f_post = substr( $f_post , 1 );
+    $f_post = substr( $f_post, 1 );
     // set header
-    if( $header != NULL ){
+    if ($header != NULL) {
         $i = 0;
-        $f_header = array();
-        foreach( $header as $key => $value ){
-            $f_header[ $i ] = "{$key}: {$value}";
+        $f_header = array ();
+        foreach ( $header as $key => $value ) {
+            $f_header [$i] = "{$key}: {$value}";
             $i ++;
         }
     }
     // setopt
-    curl_setopt( $ch , CURLOPT_POST , 1 );
-    curl_setopt( $ch , CURLOPT_POSTFIELDS , $f_post );
-    curl_setopt( $ch , CURLOPT_SSL_VERIFYPEER , FALSE );
-    curl_setopt( $ch , CURLOPT_SSL_VERIFYHOST , FALSE );
-    curl_setopt( $ch , CURLOPT_RETURNTRANSFER , 1 );
-    if( isset( $f_header ) ){
-        curl_setopt( $ch , CURLOPT_HTTPHEADER , $f_header );
+    curl_setopt( $ch, CURLOPT_POST, 1 );
+    curl_setopt( $ch, CURLOPT_POSTFIELDS, $f_post );
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, FALSE );
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, FALSE );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+    if (isset( $f_header )) {
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, $f_header );
     }
-    curl_setopt( $ch , CURLOPT_FOLLOWLOCATION , 0 );
-    curl_setopt( $ch , CURLOPT_HEADER , $header_type );
+    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 0 );
+    curl_setopt( $ch, CURLOPT_HEADER, $header_type );
     // exec
     $r = curl_exec( $ch );
-    if( curl_errno( $ch ) != 0 ){
-        throw new Exception( 'cURL error.' . curl_error( $ch ) , curl_errno( $ch ) );
+    if (curl_errno( $ch ) != 0) {
+        throw new Exception( 'cURL error.' . curl_error( $ch ), curl_errno( $ch ) );
     }
     // return
     return $r;
@@ -62,37 +61,36 @@ function WeiboPHP_cURL_Post( $url , $post , $header = NULL , $header_type = '0' 
  * @throws Exception
  * @return mixed
  */
-function WeiboPHP_cURL_Get( $url , $get , $header = NULL , $header_type = '0' )
-{
+function WeiboPHP_cURL_Get($url, $get, $header = NULL, $header_type = '0'){
     // set post string
     $f_get = '';
-    foreach( $get as $key => $value ){
+    foreach ( $get as $key => $value ) {
         $f_get = $f_get . '&' . urlencode( $key ) . '=' . urlencode( $value );
     }
-    $f_get = substr( $f_get , 1 );
+    $f_get = substr( $f_get, 1 );
     // set header
-    if( $header != NULL ){
+    if ($header != NULL) {
         $i = 0;
-        foreach( $header as $key => $value ){
-            $f_header[ $i ] = "{$key}: {$value}";
+        foreach ( $header as $key => $value ) {
+            $f_header [$i] = "{$key}: {$value}";
             $i ++;
         }
     }
     // init
     $ch = curl_init( $url . '?' . $f_get );
     // setopt
-    curl_setopt( $ch , CURLOPT_SSL_VERIFYPEER , FALSE );
-    curl_setopt( $ch , CURLOPT_SSL_VERIFYHOST , FALSE );
-    curl_setopt( $ch , CURLOPT_RETURNTRANSFER , 1 );
-    if( isset( $f_header ) ){
-        curl_setopt( $ch , CURLOPT_HTTPHEADER , $f_header );
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, FALSE );
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, FALSE );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+    if (isset( $f_header )) {
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, $f_header );
     }
-    curl_setopt( $ch , CURLOPT_FOLLOWLOCATION , 0 );
-    curl_setopt( $ch , CURLOPT_HEADER , $header_type );
+    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 0 );
+    curl_setopt( $ch, CURLOPT_HEADER, $header_type );
     // exec
     $r = curl_exec( $ch );
-    if( curl_errno( $ch ) != 0 ){
-        throw new Exception( 'cURL error.' . curl_error( $ch ) , curl_errno( $ch ) );
+    if (curl_errno( $ch ) != 0) {
+        throw new Exception( 'cURL error.' . curl_error( $ch ), curl_errno( $ch ) );
     }
     // return
     return $r;
@@ -107,34 +105,33 @@ function WeiboPHP_cURL_Get( $url , $get , $header = NULL , $header_type = '0' )
  * @throws Exception
  * @return mixed
  */
-function WeiboPHP_cURL_PostArr( $url , $post , $header = NULL , $header_type = '0' )
-{
+function WeiboPHP_cURL_PostArr($url, $post, $header = NULL, $header_type = '0'){
     // init
     $ch = curl_init( $url );
     // set header
-    if( $header != NULL ){
+    if ($header != NULL) {
         $i = 0;
-        $f_header = array();
-        foreach( $header as $key => $value ){
-            $f_header[ $i ] = "{$key}: {$value}";
+        $f_header = array ();
+        foreach ( $header as $key => $value ) {
+            $f_header [$i] = "{$key}: {$value}";
             $i ++;
         }
     }
     // setopt
-    curl_setopt( $ch , CURLOPT_POST , 1 );
-    curl_setopt( $ch , CURLOPT_POSTFIELDS , $post );
-    curl_setopt( $ch , CURLOPT_SSL_VERIFYPEER , FALSE );
-    curl_setopt( $ch , CURLOPT_SSL_VERIFYHOST , FALSE );
-    curl_setopt( $ch , CURLOPT_RETURNTRANSFER , 1 );
-    if( isset( $f_header ) ){
-        curl_setopt( $ch , CURLOPT_HTTPHEADER , $f_header );
+    curl_setopt( $ch, CURLOPT_POST, 1 );
+    curl_setopt( $ch, CURLOPT_POSTFIELDS, $post );
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, FALSE );
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, FALSE );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+    if (isset( $f_header )) {
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, $f_header );
     }
-    curl_setopt( $ch , CURLOPT_FOLLOWLOCATION , 0 );
-    curl_setopt( $ch , CURLOPT_HEADER , $header_type );
+    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 0 );
+    curl_setopt( $ch, CURLOPT_HEADER, $header_type );
     // exec
     $r = curl_exec( $ch );
-    if( curl_errno( $ch ) != 0 ){
-        throw new Exception( 'cURL error.' . curl_error( $ch ) , curl_errno( $ch ) );
+    if (curl_errno( $ch ) != 0) {
+        throw new Exception( 'cURL error.' . curl_error( $ch ), curl_errno( $ch ) );
     }
     // return
     return $r;
